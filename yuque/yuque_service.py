336e26e9c828b2778305f4d485eecc0898e2ad15
x7peeps <421614066@qq.com>
2023-03-28 09:47:59 +0800

    add fix bug
    修复没办法导出不同空间的，原因在于所有的hosturl都预设成了www.yuque，调整了一下请参考

--- yuque/yuque_service.py
+++ yuque/yuque_service.py
@@ -12,15 +12,17 @@ global COOKIE
 load_dotenv()
 COOKIE = os.environ["yuque_cookie"]
 logger = log_utils.get_logger('yuque_service')
+COOKIE = "your token"
+CSRFTOKEN= "your token"
 
-COOKIE = "lang=zh-cn; _yuque_session=Wp4jcnzLxQz_Ipaz8d5WOIbDK6rjWrW3UaubRIV7wlDEe1r9GNYNaHBUNpuO4hgu4wA1PUsJCSCncmwTojdXsw==; yuque_ctoken=jKGqoi-6Ikel-ouyXdSkvc5d; acw_tc=0bca293616755015492492845ecd49ad61620af558b22f48b3c9d7f19c19b3"
-hostUrl = "https://www.yuque.com"
+hostUrl = "https://yourrepo.yuque.com" #想要下载的空间
 
 headers = {"Accept": "application/json",
            "Content-Type": "application/json",
            "cookie": COOKIE,
            # x-csrf-token取cookie中的yuque_ctoken的值
-           "x-csrf-token": "jKGqoi-6Ikel-ouyXdSkvc5d"
+           "x-csrf-token": CSRFTOKEN,
+           "referer": "https://www.yuque.com/"
            }
 download_headers = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -34,7 +36,8 @@ download_headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     "cookie": COOKIE,
     # x-csrf-token取cookie中的yuque_ctoken的值
-    "x-csrf-token": "jKGqoi-6Ikel-ouyXdSkvc5d"
+    "x-csrf-token": CSRFTOKEN,
+    "referer": "https://www.yuque.com/"
     }
 
 
@@ -57,7 +60,8 @@ def get_blog_id_dict(catalog_resp={}):
 def getMarkDownFileExport(blog_id_dict: {}):
     markdown_file_export_dict = {}
     for blog_id, blog_index in blog_id_dict.items():
-        markdown_file_export = "https://www.yuque.com/api/docs/" + str(blog_id) + "/export"
+        # markdown_file_export = "https://www.yuque.com/api/docs/" + str(blog_id) + "/export"
+        markdown_file_export = hostUrl+"/api/docs/" + str(blog_id) + "/export"
         markdown_file_export_dict[blog_id] = markdown_file_export
     return markdown_file_export_dict
 
@@ -82,7 +86,8 @@ def getMarkDownFileDownload(markdown_file_export_dict: {}):
 
 def publish_blog(blog_id_list: []):
     for blog_id in blog_id_list:
-        markdown_file_export = "https://www.yuque.com/api/docs/" + str(blog_id) + "/publish"
+        #markdown_file_export = "https://www.yuque.com/api/docs/" + str(blog_id) + "/publish" 
+        markdown_file_export = hostUrl+"/api/docs/" + str(blog_id) + "/publish"
         req_data = {"force": False, "notify": False, "cover": None, "ignoreGlobalMessage": True}
         response = http_utils.put(markdown_file_export, data=req_data, headers=headers)
         logger.debug("publish_blog resp data:%s", response)
@@ -207,7 +212,8 @@ def convert_list_to_dict(req_list: [], key: ''):
 # GET
 # param = {"book_id": 6905122, "format": "list", "node_uuid": "qIGFgI5-T-qA2joE", "action": "edit", "title": "kkkk"}
 def get_catalog_node_list(lib_id: ''):
-    url = "https://www.yuque.com/api/catalog_nodes"
+    #url = "https://www.yuque.com/api/catalog_nodes"
+    url = hostUrl+"/api/catalog_nodes"
     param = {"book_id": lib_id}
     response = http_utils.get(url, param, headers=headers)
     logger.debug("get_catalog_node_list resp data:%s", json.dumps(response))
@@ -217,7 +223,8 @@ def get_catalog_node_list(lib_id: ''):
 
 
 def get_lib_dict():
-    url = "https://www.yuque.com/api/mine/common_used?"
+    # url = "https://www.yuque.com/api/mine/common_used?"
+    url = hostUrl+"/api/mine/common_used?"
     resp_data = http_utils.get(url, None, headers)
     # 查询返回示例
     # {"meta":{"changed":true},"data":{"groups":[],"books":[{"id":4861874441,"user_id":8432833,"organization_id":0,"type":"Book","icon":null,"title":"默认知识库","url":"/go/book/6905092","order_num":0,"target_id":6905092,"target_type":"Book","created_at":"2022-04-07T14:08:45.000Z","updated_at":"2022-04-09T05:47:47.000Z","ref_id":"dashboard_books","target":{"id":6905092,"type":"Book","slug":"kb","name":"默认知识库","user_id":8432833,"description":null,"creator_id":8432833,"public":0,"scene":null,"created_at":"2020-12-22T12:27:47.000Z","updated_at":"2023-02-06T11:32:26.000Z","content_updated_at":"2022-05-11T01:10:06.509Z","archived_at":null,"organization_id":0,"enable_auto_publish":true,"privacy_migrated":true,"user":null,"_serializer":"web.book_lite"},"user":{"id":8432833,"type":"User","login":"rainco","name":"Rainco","avatar":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","scene":null,"avatar_url":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","role":1,"isPaid":false,"member_level":1,"followers_count":0,"following_count":2,"description":"🚜☁️☁️","status":1,"_serializer":"web.user_lite"},"_serializer":"web.quick_link"},{"id":4861874444,"user_id":8432833,"organization_id":0,"type":"Book","icon":null,"title":"学习笔记","url":"/go/book/6905122","order_num":1,"target_id":6905122,"target_type":"Book","created_at":"2022-04-07T14:08:45.000Z","updated_at":"2022-04-09T05:47:47.000Z","ref_id":"dashboard_books","target":{"id":6905122,"type":"Book","slug":"zzhta4","name":"学习笔记","user_id":8432833,"description":"点滴学习，随时记录","creator_id":8432833,"public":0,"scene":"notes","created_at":"2020-12-22T12:35:01.000Z","updated_at":"2023-02-06T14:04:01.000Z","content_updated_at":"2023-02-06T14:04:00.814Z","archived_at":null,"organization_id":0,"enable_auto_publish":false,"privacy_migrated":true,"user":null,"_serializer":"web.book_lite"},"user":{"id":8432833,"type":"User","login":"rainco","name":"Rainco","avatar":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","scene":null,"avatar_url":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","role":1,"isPaid":false,"member_level":1,"followers_count":0,"following_count":2,"description":"🚜☁️☁️","status":1,"_serializer":"web.user_lite"},"_serializer":"web.quick_link"},{"id":4861874446,"user_id":8432833,"organization_id":0,"type":"Book","icon":null,"title":"Maycur","url":"/go/book/20978116","order_num":2,"target_id":20978116,"target_type":"Book","created_at":"2022-04-07T14:08:45.000Z","updated_at":"2022-04-09T05:47:47.000Z","ref_id":"dashboard_books","target":{"id":20978116,"type":"Book","slug":"fpbksm","name":"Maycur","user_id":8432833,"description":"","creator_id":8432833,"public":0,"scene":null,"created_at":"2021-09-28T03:01:13.000Z","updated_at":"2023-02-07T07:15:50.000Z","content_updated_at":"2023-02-06T01:55:00.659Z","archived_at":null,"organization_id":0,"enable_auto_publish":true,"privacy_migrated":true,"user":null,"_serializer":"web.book_lite"},"user":{"id":8432833,"type":"User","login":"rainco","name":"Rainco","avatar":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","scene":null,"avatar_url":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","role":1,"isPaid":false,"member_level":1,"followers_count":0,"following_count":2,"description":"🚜☁️☁️","status":1,"_serializer":"web.user_lite"},"_serializer":"web.quick_link"},{"id":4861874448,"user_id":8432833,"organization_id":0,"type":"Book","icon":null,"title":"技术栈","url":"/go/book/20684276","order_num":4,"target_id":20684276,"target_type":"Book","created_at":"2022-04-07T14:08:45.000Z","updated_at":"2022-04-09T05:47:47.000Z","ref_id":"dashboard_books","target":{"id":20684276,"type":"Book","slug":"bdreeb","name":"Tech Stack","user_id":8432833,"description":"","creator_id":8432833,"public":0,"scene":null,"created_at":"2021-08-31T03:51:12.000Z","updated_at":"2023-01-21T04:17:47.000Z","content_updated_at":"2023-01-21T04:17:47.112Z","archived_at":null,"organization_id":0,"enable_auto_publish":true,"privacy_migrated":true,"user":null,"_serializer":"web.book_lite"},"user":{"id":8432833,"type":"User","login":"rainco","name":"Rainco","avatar":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","scene":null,"avatar_url":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","role":1,"isPaid":false,"member_level":1,"followers_count":0,"following_count":2,"description":"🚜☁️☁️","status":1,"_serializer":"web.user_lite"},"_serializer":"web.quick_link"},{"id":4861874449,"user_id":8432833,"organization_id":0,"type":"Book","icon":null,"title":"Self Media","url":"/go/book/24750617","order_num":5,"target_id":24750617,"target_type":"Book","created_at":"2022-04-07T14:08:45.000Z","updated_at":"2022-04-09T05:47:47.000Z","ref_id":"dashboard_books","target":{"id":24750617,"type":"Book","slug":"abix3e","name":"Self Media","user_id":8432833,"description":"","creator_id":8432833,"public":0,"scene":null,"created_at":"2022-02-13T04:48:43.000Z","updated_at":"2022-06-08T17:17:55.000Z","content_updated_at":"2022-02-13T05:55:35.868Z","archived_at":null,"organization_id":0,"enable_auto_publish":true,"privacy_migrated":true,"user":null,"_serializer":"web.book_lite"},"user":{"id":8432833,"type":"User","login":"rainco","name":"Rainco","avatar":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","scene":null,"avatar_url":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","role":1,"isPaid":false,"member_level":1,"followers_count":0,"following_count":2,"description":"🚜☁️☁️","status":1,"_serializer":"web.user_lite"},"_serializer":"web.quick_link"},{"id":5940143903,"user_id":8432833,"organization_id":0,"type":"Book","icon":null,"title":"Diary","url":"/go/book/27780490","order_num":12,"target_id":27780490,"target_type":"Book","created_at":"2022-11-17T16:00:04.000Z","updated_at":"2022-11-17T16:00:04.000Z","ref_id":"dashboard_books","target":{"id":27780490,"type":"Book","slug":"gosofe","name":"Diary","user_id":8432833,"description":"","creator_id":8432833,"public":0,"scene":null,"created_at":"2022-05-11T01:09:08.000Z","updated_at":"2022-06-08T20:11:20.000Z","content_updated_at":"2022-05-11T01:10:42.914Z","archived_at":null,"organization_id":0,"enable_auto_publish":true,"privacy_migrated":true,"user":null,"_serializer":"web.book_lite"},"user":{"id":8432833,"type":"User","login":"rainco","name":"Rainco","avatar":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","scene":null,"avatar_url":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","role":1,"isPaid":false,"member_level":1,"followers_count":0,"following_count":2,"description":"🚜☁️☁️","status":1,"_serializer":"web.user_lite"},"_serializer":"web.quick_link"},{"id":5940143904,"user_id":8432833,"organization_id":0,"type":"Book","icon":null,"title":"On Java 8","url":"/go/book/32207768","order_num":13,"target_id":32207768,"target_type":"Book","created_at":"2022-11-17T16:00:04.000Z","updated_at":"2022-11-17T16:00:04.000Z","ref_id":"dashboard_books","target":{"id":32207768,"type":"Book","slug":"ex0xli","name":"On Java 8","user_id":8432833,"description":"","creator_id":8432833,"public":0,"scene":null,"created_at":"2022-08-24T07:19:54.000Z","updated_at":"2022-08-24T07:27:56.000Z","content_updated_at":"2022-08-24T07:27:56.308Z","archived_at":null,"organization_id":0,"enable_auto_publish":true,"privacy_migrated":true,"user":null,"_serializer":"web.book_lite"},"user":{"id":8432833,"type":"User","login":"rainco","name":"Rainco","avatar":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","scene":null,"avatar_url":"https://cdn.nlark.com/yuque/0/2021/png/8432833/1632801197519-avatar/a24027c5-29fd-4951-afa2-be5ab015a178.png","role":1,"isPaid":false,"member_level":1,"followers_count":0,"following_count":2,"description":"🚜☁️☁️","status":1,"_serializer":"web.user_lite"},"_serializer":"web.quick_link"}]}}
